9ebb1a2203b38ac6ddd95707c23b36576b7514db
Adam Watkins <adam@stupidpupil.co.uk>
2024-10-27 17:32:53 +0000

    Add Sapphic Bison

--- main.js
+++ main.js
@@ -473,6 +473,7 @@ $(function(){
   load_exchange_url("https://stupidpupil.github.io/medichecks_scraper/exchange.json")
   load_exchange_url("https://stupidpupil.github.io/blue_horizon_scraper/exchange.json")
   load_exchange_url("https://stupidpupil.github.io/randox_scraper/exchange.json")
+  load_exchange_url("https://stupidpupil.github.io/sapphic_bison_scraper/exchange.json")
 
   $("#biomarkers-select").on("change", resolve)
   $("#require-venous-checkbox").on("change", resolve)
